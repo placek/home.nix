9cbe9141e4fc93673250af8b4f52581b3b35902e
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-03-28 10:09:04 +0200

    First attempt to xpass

--- packages/default.nix
+++ packages/default.nix
@@ -4,8 +4,41 @@ let
 in
 with pkgs; [
   glpkgs.nixGLIntel
-  (pkgs.writeShellScriptBin "qutebrowser-gl" ''${glpkgs.nixGLIntel}/bin/nixGLIntel qutebrowser "$@"'')
-  (pkgs.writeShellScriptBin "kitty-gl" ''${glpkgs.nixGLIntel}/bin/nixGLIntel kitty "$@"'')
+  (writeShellScriptBin "qutebrowser-gl" ''${glpkgs.nixGLIntel}/bin/nixGLIntel qutebrowser "$@"'')
+  (writeShellScriptBin "kitty-gl" ''${glpkgs.nixGLIntel}/bin/nixGLIntel kitty "$@"'')
+  (writeShellScriptBin "xpass" ''
+    case "$1" in
+      a*)
+        entry=$(find -L ${settings.key.store} -name "*.gpg" -type "f" -printf "%P\n" | sed 's/\.gpg$//' | xprompt -c)
+        pass_output=$(pass "$entry" | sed '1s/^/password: /' | sed 's/^otpauth:/otp: &/')
+        password=$(echo "$pass_output" | yq ".password")
+        user=$(echo "$pass_output" | yq ".user")
+        pass otp --clip "$entry"
+        ydotool type "$user\t$password"
+        ;;
+      u*)
+        entry=$(find -L ${settings.key.store} -name "*.gpg" -type "f" -printf "%P\n" | sed 's/\.gpg$//' | xprompt -c)
+        pass_output=$(pass "$entry" | sed '1s/^/password: /' | sed 's/^otpauth:/otp: &/')
+        user=$(echo "$pass_output" | yq ".user")
+        ydotool type "$user"
+        ;;
+      p*)
+        entry=$(find -L ${settings.key.store} -name "*.gpg" -type "f" -printf "%P\n" | sed 's/\.gpg$//' | xprompt -c)
+        pass_output=$(pass "$entry" | sed '1s/^/password: /' | sed 's/^otpauth:/otp: &/')
+        password=$(echo "$pass_output" | yq ".password")
+        pass otp --clip "$entry"
+        ydotool type "$password"
+        ;;
+      o*)
+        entry=$(find -L ${settings.key.store} -name "*.gpg" -type "f" -printf "%P\n" | sed 's/\.gpg$//' | xprompt -c)
+        pass otp --clip "$entry"
+        ;;
+      *)
+        >&2 echo "xpass help|autocpmplete|user|password|otp"
+        exit 1
+        ;;
+    esac
+  '')
   (nerdfonts.override { fonts = [ settings.font.name ]; })
   (import ./git-ctags.nix { inherit pkgs; })
 
@@ -38,5 +71,6 @@ with pkgs; [
   unzip
   wget
   ydotool
+  yq
   wl-clipboard
 ]
