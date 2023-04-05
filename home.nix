e4512817835fb757860ed40abfae4470ec3bcef9
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-04-05 10:27:12 +0200

    Add ydotool daemon service

--- home.nix
+++ home.nix
@@ -20,4 +20,19 @@ in
   home.file = import ./files;
   programs = import ./programs { inherit pkgs; };
   services = import ./services { inherit pkgs; };
+
+  systemd.user.services = {
+    ydotoold = {
+      Unit = {
+        Description = "ydotool daemon with keyboard only";
+        Documentation = [ "man:ydotoold(1)" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.ydotool}/bin/ydotoold --mouse-off --touch-off";
+      };
+      Install = {
+        WantedBy = [ "gnome-session.target" ];
+      };
+    };
+  };
 }
