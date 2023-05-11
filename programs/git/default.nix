c9136b2271160d5514b679fd29d5410c114b4e94
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-05-11 07:53:34 +0200

    Fixes for git configuration

--- programs/git/default.nix
+++ programs/git/default.nix
@@ -43,16 +43,19 @@ in
     info = "for-each-ref --format='%(color:yellow)%(committerdate:iso8601) %(color:blue)%(objectname:short) %(color:magenta)%(authorname) %(color:green)%(refname:short) %(color:reset)%(contents:subject)' --sort -committerdate";
 
     # restore
-    r = "restore";
-    rs = "restore --staged";
+    undo = "restore";
+    undos = "restore --staged";
 
     # reset
-    r1 = "reset --soft @~1";
-    r2 = "reset --soft @~2";
-    r3 = "reset --soft @~3";
+    rs1 = "reset --soft @~1";
+    rs2 = "reset --soft @~2";
+    rs3 = "reset --soft @~3";
     rh1 = "reset --hard @~1";
     rh2 = "reset --hard @~2";
     rh3 = "reset --hard @~3";
+    rm1 = "reset --mixed @~1";
+    rm2 = "reset --mixed @~2";
+    rm3 = "reset --mixed @~3";
   };
   ignores = [
     "*.key"
@@ -61,27 +64,27 @@ in
     ".DS_Store"
     ".authorized_keys"
     ".local.*"
-    ".remote.compose"
+    ".remote.*"
     ".sw[op]"
     ".tags"
     "nohup.out"
   ];
   extraConfig = {
-    commit.verbose = true;
-    github.user = settings.user.name;
     apply.whitespace = "nowarn";
-    rerere.enabled = 1;
-    merge.tool = "vimdiff";
+    commit.verbose = true;
+    core.attributesfile = "${settings.dirs.home}/.gitattributes_global";
+    core.editor = editor;
+    core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
     difftool.prompt = false;
-    mergetool.trustExitCode = true;
-    mergetool.conflictstyle = "diff3";
-    pull.ff = "only";
     difftool.trustExitCode = true;
+    fetch.prune = true;
+    github.user = settings.user.name;
+    merge.tool = "vimdiff";
+    mergetool.conflictstyle = "diff3";
+    mergetool.trustExitCode = true;
+    pull.ff = "only";
+    rebase.autosquash = true;
+    rerere.enabled = 1;
     status.submoduleSummary = true;
-    core = {
-      editor = editor;
-      attributesfile = "${settings.dirs.home}/.gitattributes_global";
-      whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
-    };
   };
 }
