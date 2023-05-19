7a758193349f155d22d9ca5ff7e675e94ce2f3c8
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-05-19 10:03:44 +0200

    add traditional lectionary to bookmarks

--- home/files/default.nix
+++ home/files/default.nix
@@ -29,24 +29,28 @@
     enable = true;
     target = "${settings.dirs.home}/.config/qutebrowser/bookmarks/urls";
     text = ''
-      https://docs.servant.dev/en/stable/ Servant docs
       https://nixos.org/manual/nix/unstable/introduction.html Nix refs
       https://nixos.wiki/wiki/Cheatsheet NixOS cheatsheet
       https://nixos.org/manual/nixos/stable/index.html NixOS man
-      https://learnvimscriptthehardway.stevelosh.com/ Vimscript tutor
+
+      https://docs.servant.dev/en/stable/ Servant docs
       https://www.stephendiehl.com/ Shephen Diehl homepage
       https://haskell-at-work.com/episodes.html Haskell at Work
       https://crypto.stanford.edu/~blynn/haskell/ Haskell Fan Site
-      http://www.ultramontes.pl/index.htm Ultra montes
-      https://bibles-online.net/1550/ Stephanus Greek New Testament
-      https://spotlight.vatlib.it/greek-paleography/feature/1-majuscule-bookhands Greek paleography
-      https://imgflip.com/memegenerator Meme Generator
       https://reflex-frp.org/#try-reflex-frp Reflex FRP
       https://jproyo.github.io/posts/2021-03-17-encoding-effects-freer-simple.html Encoding Effects using freer-simple
       https://hackage.haskell.org/package/polysemy Polysemy Haskell
+
       https://ergodox-ez.com/pages/oryx-planck Oryx Planck
+      https://learnvimscriptthehardway.stevelosh.com/ Vimscript tutor
       https://feed-web.binarapps.com/dashboard BinarFeed
+      https://imgflip.com/memegenerator Meme Generator
       http://www.gifntext.com/ Gif&Text
+
+      http://www.ultramontes.pl/index.htm Ultra montes
+      http://www.lectionarycentral.com/index.html Traditional lectionary
+      https://bibles-online.net/1550/ Stephanus Greek New Testament
+      https://spotlight.vatlib.it/greek-paleography/feature/1-majuscule-bookhands Greek paleography
     '';
   };
 
