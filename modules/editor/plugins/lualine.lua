8702b24c590dcbce5238df8da8bb14d8e7c3e5c2
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-07-14 21:47:51 +0200

    One-char mode in lualine

--- modules/editor/plugins/lualine.lua
+++ modules/editor/plugins/lualine.lua
@@ -7,7 +7,7 @@ require("lualine").setup {
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
     lualine_b = {
       "branch",
       "diff",
