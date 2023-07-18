d8314d8da5aac5b37ca0bbbc97466b031624d8c4
placek <placzynski.pawel@gmail.com>
2023-07-18 12:26:38 +0200

    Fix fugitive status show

--- modules/editor/plugins/fugitive.lua
+++ modules/editor/plugins/fugitive.lua
@@ -1,13 +1,11 @@
 local function show_fugitive()
-  if vim.fn.FugitiveHead() ~= "" then
-    vim.cmd [[
+  vim.cmd [[
     Git
     resize 10
     setlocal winfixheight
     setlocal nonumber
     setlocal norelativenumber
-    ]]
-  end
+  ]]
 end
 
 toggle_fugitive = function()
