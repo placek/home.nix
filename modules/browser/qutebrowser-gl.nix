1565f866b14c85a9479b7254bf91ef8f9371a5d5
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-08-21 15:44:33 +0200

    Even more cleanup

--- modules/browser/qutebrowser-gl.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs
-, glpkgs
-, ...
-}:
-pkgs.writeShellScriptBin "qutebrowser-gl" ''
-  #!${pkgs.stdenv.shell}
-  export ALSA_PLUGIN_DIR=${pkgs.pipewire.lib}/lib/alsa-lib
-  ${glpkgs.nixGLIntel}/bin/nixGLIntel qutebrowser $@
-''
