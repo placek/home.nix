8b14e2c04ccc89c5fbdb02227877f7ea574dba3f
Paweł Placzyński <placzynski.pawel@gmail.com>
2023-12-29 07:29:44 +0100

    Use standard spotify package

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -79,11 +79,8 @@
       lilypond
       qcad
       slack
+      spotify
       steam
-
-      (import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8.tar.gz";
-      }) {}).spotify
     ];
   };
 }
