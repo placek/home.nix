5cb16ff8511bd7872fd77e339f005b5681946579
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-05-09 12:14:59 +0200

    Fix the sha checksum for vasm
    This commit corrects the sha checksum for the vasm package in the utils
    module. The previous sha256 value was incorrect, and it has been updated
    to the appropriate value to ensure the integrity and security of the
    ackage.

--- modules/utils/vasm.nix
+++ modules/utils/vasm.nix
@@ -9,7 +9,7 @@ pkgs.stdenv.mkDerivation rec {
     owner  = "mbitsnbites";
     repo   = "vasm-mirror";
     rev    = "2dce5498402353bbe01d74949fe765ee3620d6d5";
-    sha256 = "0zrv1fk8ilijdqksg3l0al4yszb1g2ppcp25rjxidrkxyc79zaxr";
+    sha256 = "sha256-GehSxHSlTPzOUq+YiZYpmGJyQBv0ufJl7CuboBJ+l3M=";
   };
 
   makeFlags = [ "CPU=6502" "SYNTAX=oldstyle" ];
