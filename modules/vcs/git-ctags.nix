06b7d1ef55b9063fe29b09c766290cd5f0537ea8
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-05-22 12:14:19 +0200

    Set global git-ctags implementation for stability
    By modifying the 'modules/vcs/git-ctags.nix' file, the commit sets a
    global implementation for git-ctags. The updated script simplifies the
    process by directly generating and moving tag files using ctags in a
    more robust manner. The previous logic that checked for hooks has been
    replaced with a straight-forward approach, enhancing the overall
    stability and efficiency of handling ctags in Git repositories.

--- modules/vcs/git-ctags.nix
+++ modules/vcs/git-ctags.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ pkgs
+, ...
+}:
 pkgs.writeShellScriptBin "git-ctags" ''
-  #!${pkgs.stdenv.shell}
   set -e
 
-  if [ -x ".git/hooks/ctags" ]; then
-    .git/hooks/ctags
-  else
-    >&2 echo "git-ctags: no hook at .git/hooks/ctags exists"
-  fi
+  dir="$(git rev-parse --git-dir)"
+  trap 'rm -f "$dir/$$.tags"' EXIT
+  git ls-files | ctags --tag-relative -G -L - -f "$dir/$$.tags" 2> /dev/null
+  mv "$dir/$$.tags" "$dir/tags"
 ''
