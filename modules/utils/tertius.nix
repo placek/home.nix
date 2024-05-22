422cbdc7eef14e3db110199b7ea8b030b5f3d1b4
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-05-22 09:10:29 +0200

    Refactor tertius.nix for greater flexibility
    In this commit, the tertius.nix file was updated to enhance its
    flexibility in usage within the environment by utilizing environment
    variables. Specifically, the logic for setting the model and language
    parameters was adjusted to make use of environment variables
    OPENAI_MODEL and OPENAI_LANGUAGE, respectively. This change allows for
    more dynamic customization of the parameters used by the tertius script,
    enabling a more versatile and adaptable usage experience.

--- modules/utils/tertius.nix
+++ modules/utils/tertius.nix
@@ -8,23 +8,22 @@ pkgs.writeShellScriptBin "tertius" ''
     exit 1
   fi
 
-  model="''${model:-gpt-3.5-turbo}"
-  payload_template='{ model: $model, temperature: 1, max_tokens: 4096, top_p: 1, frequency_penalty: 0, presence_penalty: 0, messages: $data }'
+  model="''${OPENAI_MODEL:-gpt-3.5-turbo}"
+  language="''${OPENAI_LANGUAGE:-english}"
+  payload_template='{ model: $model, temperature: 1, max_tokens: 4095, top_p: 1, frequency_penalty: 0, presence_penalty: 0, messages: $data }'
   data=""
 
   usage() {
-    >&2 echo "Usage: tertius [-m \"model\"] ask"
-    >&2 echo "       tertius [-m \"model\"] grammar"
-    >&2 echo "       tertius [-m \"model\"] story"
-    >&2 echo "       tertius [-m \"model\"] commit-message"
-    >&2 echo "       tertius [-m \"model\"] pull-request"
+    >&2 echo "Usage: tertius ask"
+    >&2 echo "       tertius grammar"
+    >&2 echo "       tertius story"
+    >&2 echo "       tertius commit-message"
+    >&2 echo "       tertius pull-request"
+    >&2 echo "       tertius report"
   }
 
-  while getopts ":m:" arg; do
+  while getopts ":" arg; do
     case $arg in
-    m )
-      model=$OPTARG
-      ;;
     \? )
       >&2 echo "tertius: invalid option: -$OPTARG."
       usage
@@ -46,6 +45,7 @@ pkgs.writeShellScriptBin "tertius" ''
   fi
 
   command=$1
+  instructions="Formulate all answers in $language. "
 
   case $command in
   ask )
@@ -53,7 +53,7 @@ pkgs.writeShellScriptBin "tertius" ''
     ;;
 
   grammar )
-    instructions="Correct the grammar of the following text. Ensure that the text is free of spelling and grammatical errors. The goal is to produce a clear, concise, and grammatically correct text. If the text is already correct, just output it."
+    instructions+="Correct the grammar of the following text. Ensure that the text is free of spelling and grammatical errors. The goal is to produce a clear, concise, and grammatically correct text. If the text is already correct, just output it."
 
     data=$(${pkgs.jq}/bin/jq -n --arg question "$(cat)" --arg instructions "$instructions" '[ { role: "system", content: $instructions }, { role: "user", content: $question } ]')
     ;;
@@ -65,11 +65,11 @@ pkgs.writeShellScriptBin "tertius" ''
     issue=$(${pkgs.git}/bin/git rev-parse --abbrev-ref HEAD | ${pkgs.gnused}/bin/sed -n 's@.*/\([[:digit:]]\+\).*@\1@p')
 
     if [ -n "$issue" ]; then
-      instructions="Compose a Git commit message. To draft a Git commit message, first analyze the user story to understand why changes were needed. Next, review the series of commit messages to see what modifications have been made.  Concentrate on the last item, considering it the commit candidate. This should include a brief explanation of the changes and a diff that showcases these modifications. Your task is to ensure that there is a clear connection between the requirements specified in the user story and the changes made in the commit. The objective is to create a concise and informative commit message that effectively communicates the reasoning behind the changes and high-level explanation of the alterations. The message should comprise a succinct title, followed by two paragraphs: one explaining the reason for the changes and another describing the changes themselves. Each section should be separated by a blank line, without any headings."
+      instructions+="Compose a Git commit message. To draft a Git commit message, first analyze the user story to understand why changes were needed. Next, review the series of commit messages to see what modifications have been made.  Concentrate on the last item, considering it the commit candidate. This should include a brief explanation of the changes and a diff that showcases these modifications. Your task is to ensure that there is a clear connection between the requirements specified in the user story and the changes made in the commit. The objective is to create a concise and informative commit message that effectively communicates the reasoning behind the changes and high-level explanation of the alterations. The message should comprise a succinct title, followed by two paragraphs: one explaining the reason for the changes and another describing the changes themselves. Each section should be separated by a blank line, without any headings."
       context=$(${pkgs.gh}/bin/gh issue view --json title,body $issue | ${pkgs.jq}/bin/jq  "\"\(.title)\n\n\(.body)\"")
       data=$(${pkgs.jq}/bin/jq -n --arg instructions "$instructions" --arg context "This is the user story:\n$context" '[ { role: "system", content: $instructions }, { role: "user", content: $context } ]')
     else
-      instructions="Compose a Git commit message. To draft a Git commit message, review the series of commit messages to see what modifications have been made.  Concentrate on the last item, considering it the commit candidate. This should include a brief explanation of the changes and a diff that showcases these modifications. Your task is to ensure that there is a clear connection between the changes made in the commits. The objective is to create a concise and informative commit message that effectively communicates the reasoning behind the changes and details the specific alterations. The message should comprise a succinct title, followed by two paragraphs: one explaining the reason for the changes and another describing the changes themselves. Each section should be separated by a blank line, without any headings."
+      instructions+="Compose a Git commit message. To draft a Git commit message, review the series of commit messages to see what modifications have been made.  Concentrate on the last item, considering it the commit candidate. This should include a brief explanation of the changes and a diff that showcases these modifications. Your task is to ensure that there is a clear connection between the changes made in the commits. The objective is to create a concise and informative commit message that effectively communicates the reasoning behind the changes and details the specific alterations. The message should comprise a succinct title, followed by two paragraphs: one explaining the reason for the changes and another describing the changes themselves. Each section should be separated by a blank line, without any headings."
       data=$(${pkgs.jq}/bin/jq -n --arg instructions "$instructions" '[ { role: "system", content: $instructions } ]')
     fi
 
@@ -82,13 +82,13 @@ pkgs.writeShellScriptBin "tertius" ''
     ;;
 
   story )
-    instructions="Compose a problem description by analyzing a short explanation of the problem, and additionally, any relevant context or background information. Ensure a thorough understanding of the problem and the context in which it occurs. The goal is to generate a clear, concise problem description that provides all the necessary information to understand the problem and its context. The problem description should have a short title, a paragraph with user story formatted scenario (As <actor>, I want to <action>, so <outcome>.), a 'Summary' paragraph explaining the problem, and an 'Acceptance criteria' paragraph with the tasks that has to be done to solve problem."
+    instructions+="Compose a problem description by analyzing a short explanation of the problem, and additionally, any relevant context or background information. Ensure a thorough understanding of the problem and the context in which it occurs. The goal is to generate a clear, concise problem description that provides all the necessary information to understand the problem and its context. The problem description should have a short title, a paragraph with user story formatted scenario (As <actor>, I want to <action>, so <outcome>.), a 'Summary' paragraph explaining the problem, and an 'Acceptance criteria' paragraph with the tasks that has to be done to solve problem."
 
     data=$(${pkgs.jq}/bin/jq -n --arg question "$(cat)" --arg instructions "$instructions" '[ { role: "system", content: $instructions }, { role: "user", content: $question } ]')
     ;;
 
   pull-request )
-    instructions="Compose a pull request description by analyzing a user story, and additionally, any commit message that follows. Ensure a thorough understanding of the changes and the context in which they occur. The goal is to generate a clear, concise pull request description that provides all the necessary information to understand the changes and their context. The pull request description should have a paragraph explaining the purpose of the changes, and a paragraph explaining the outome of the changes themselves - each such component has to be separated by two newlines and have no header."
+    instructions+="Compose a pull request description by analyzing a user story, and additionally, any commit message that follows. Ensure a thorough understanding of the changes and the context in which they occur. The goal is to generate a clear, concise pull request description that provides all the necessary information to understand the changes and their context. The pull request description should have a paragraph explaining the purpose of the changes, and a paragraph explaining the outome of the changes themselves - each such component has to be separated by two newlines and have no header."
 
     default_branch=$(${pkgs.git}/bin/git config --get core.default)
     branchoff_commit=$(${pkgs.git}/bin/git merge-base $default_branch HEAD)
