e0401057b8cf4d4a08c14e32ecf97de6149de34f
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-07-05 09:32:44 +0200

    Add enhanced Rome Total War to games
    This commit introduces the wine-rome-total-war-enhancement package to
    the games module by including it in the games.nix file. The new package,
    "rome-total-war-enhancement", enhances the gameplay experience by
    launching Rome Total War with custom settings using the specified mod
    and command-line options. By integrating this enhancement, users can
    enjoy an improved and tailored gaming experience within the Rome Total
    War environment.

--- modules/gui/games.nix
+++ modules/gui/games.nix
@@ -22,8 +22,16 @@ let
   wine-rome-total-war-alexander = import ./run_in_wine.nix {
     inherit pkgs config;
     name = "rome-total-war-alexander";
+    prefix = "rome-total-war";
     path = "Program Files (x86)/The Creative Assembly/Rome - Total War";
     exe  = "RomeTW-ALX.exe";
+  };
+
+  wine-rome-total-war-enhancement = import ./run_in_wine.nix {
+    inherit pkgs config;
+    name = "rome-total-war-enhancement";
+    path = "Program Files (x86)/The Creative Assembly/Rome - Total War";
+    exe  = "RomeTW-ALX.exe -show_err -mod:HRTW -noalexander";
   };
 
   dwarf-fortress = (import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/3f888f673360440b6b7e6994a1e6d092d7588e84.tar.gz"; }) {}).pkgs.dwarf-fortress.override {
@@ -37,6 +45,7 @@ in
       wine-rome-total-war
       wine-rome-total-war-alexander
       wine-rome-total-war-barbarian-invasion
+      wine-rome-total-war-enhancement
       dwarf-fortress
 
       pkgs.wineWowPackages.stable
--- modules/gui/run_in_wine.nix
+++ modules/gui/run_in_wine.nix
@@ -8,5 +8,5 @@
 pkgs.writeShellScriptBin "wine-${name}" ''
   export WINEPREFIX="${config.home.homeDirectory}/Wine/${prefix}"
   cd "${config.home.homeDirectory}/Wine/${prefix}/drive_c/${path}"
-  exec wine "${exe}"
+  exec wine ${exe}
 ''
