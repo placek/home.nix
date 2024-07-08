c28d1791a7ba9155d1182e7c248dd3210cc00234
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-07-08 13:50:35 +0200

    Refactor fugitive module to be more modular
    This commit refactors the existing fugitive module to enhance its
    modularity and organization. The fugitive configuration is now
    segregated into a standalone module named `fugitive.nix`. Various
    functions and mappings specific to git operations have been restructured
    within this dedicated module. The separation of these functionalities
    from the default.nix file improves the overall management and
    independence of the fugitive setup. The modifications include creating
    the `fugitive.nix` file and updating the function/mappings references
    accordingly. This refactoring ensures a cleaner and more maintainable
    configuration, enabling better customization options for
    fugitive-related functionalities.

--- modules/editor/fugitive.nix
+++ modules/editor/fugitive.nix
@@ -11,8 +11,26 @@
 
     editor.RCs = [
       ''
-        " fugitive
-        function! s:toggleFugitive()
+        " Git helpers
+        function! s:defaultBranch() abort
+          return trim(system("${config.vcsExec} config --get core.default"))
+        endfunction
+
+        function! s:branchoffCommit(...) abort
+          if a:0 > 0
+            let l:head = a:1
+          else
+            let l:head = "HEAD"
+          end
+          return trim(system("${config.vcsExec} merge-base " . <sid>defaultBranch() . " " . l:head))
+        endfunction
+
+        function! s:whatthecommit() abort
+          return trim(system("curl -Ls whatthecommit.com/index.txt"))
+        endfunction
+
+        " toggle fugitive status window
+        function! s:gitToggleStatus() abort
           if buflisted(bufname('fugitive:///*/.git//$'))
             execute ":bdelete" bufname('fugitive:///*/.git//$')
           else
@@ -24,45 +42,41 @@
           endif
         endfunction
 
-        function! s:pickaxe(query)
+        nnoremap <silent> <Plug>(GitToggleStatus) :<c-u>call <sid>gitToggleStatus()<cr>
+
+        " pickaxe search
+        function! s:gitPickaxe(query) abort
           if !empty(a:query)
             execute ":G log -p -G \"" a:query "\""
             call setreg("/", "\\V" . a:query)
           endif
         endfunction
 
-        function! s:defaultBranch()
-          return trim(system("git config --get core.default"))
-        endfunction
+        nnoremap <silent> <Plug>(GitPickaxeWithInput) :<c-u>call <sid>gitPickaxe(input("pickaxe /", "", "tag"))<cr>
+        vnoremap <silent> <Plug>(GitPickaxeWithSelection) :call <sid>gitPickaxe(<sid>selectedText())<cr>
 
-        function! s:branchoffCommit(...)
-          if a:0 > 0
-            let l:head = a:1
-          else
-            let l:head = "HEAD"
-          end
-          return trim(system("git merge-base " . <sid>defaultBranch() . " " . l:head))
-        endfunction
-
-        function! s:whatthecommit()
-          return trim(system("curl -Ls whatthecommit.com/index.txt"))
-        endfunction
-
-        function! s:gitPruneFile(...)
+        " prune a file in whole git history
+        function! s:gitPruneFile(...) abort
           if a:0 > 0
             let l:head = a:1
           else
             let l:head = "HEAD"
           end
           let file = expand("%")
-          execute "!git filter-branch --prune-empty --force --tree-filter 'rm -f " . l:file . "' " . s:branchoffCommit(l:head) . ".." . l:head
+          execute "!${config.vcsExec} filter-branch --prune-empty --force --tree-filter 'rm -f " . l:file . "' " . s:branchoffCommit(l:head) . ".." . l:head
         endfunction
 
-        function! s:gitWIP()
+        nnoremap <silent> <Plug>(GitPruneFile) :<c-u>call <sid>gitPruneFile()<cr>
+
+        " make a WIP commit
+        function! s:gitWIP() abort
           execute ":G commit --no-verify --message 'WIP " . <sid>whatthecommit() . "'"
         endfunction
 
-        function! s:gitRebaseBranch(...)
+        nnoremap <silent> <Plug>(GitWIP) :<c-u>call <sid>gitWIP()<cr>
+
+        " rebase a feature branch
+        function! s:gitRebaseBranch(...) abort
           if a:0 > 0
             let l:head = a:1
           else
@@ -71,7 +85,10 @@
           execute ":G rebase --interactive " . <sid>branchoffCommit(l:head)
         endfunction
 
-        function! s:gitChanges(...)
+        nnoremap <silent> <Plug>(GitRebaseBranch) :<c-u>call <sid>gitRebaseBranch()<cr>
+
+        " show changes in a feature branch
+        function! s:gitChanges(...) abort
           if a:0 > 0
             let l:head = a:1
           else
@@ -80,13 +97,18 @@
           execute ":Gclog --pretty=oneline " . <sid>branchoffCommit(l:head) . ".." . l:head
         endfunction
 
-        function! s:gitUpdate()
+        nnoremap <silent> <Plug>(GitChanges) :call <sid>gitChanges()<cr>
+
+        " pull and rebase a feature branch
+        function! s:gitPullAndRebase() abort
           G fetch
           execute ":G pull --rebase " . substitute(<sid>defaultBranch(), "/", " ", "")
           echom "Branch rebased to its default branch"
         endfunction
 
-        function! s:gitAbsorb()
+        nnoremap <silent> <Plug>(GitPullAndRebase) :call <sid>gitPullAndRebase()<cr>
+
+        function! s:gitAbsorb() abort
           execute ":G absorb --base " . <sid>branchoffCommit()
         endfunction
 
@@ -94,9 +116,10 @@
           execute ":G absorb --and-rebase --base " . <sid>branchoffCommit()
         endfunction
 
-        function! s:gitPush()
+        " push with force
+        function! s:gitPushForce() abort
           execute ":G push --force-with-lease --follow-tags"
-          let l:branch = trim(system("git rev-parse --abbrev-ref HEAD"))
+          let l:branch = trim(system("${config.vcsExec} rev-parse --abbrev-ref HEAD"))
           let l:base = substitute(<sid>defaultBranch(), ".*/", "", "")
           echom "Branch pushed"
 
@@ -107,49 +130,53 @@
           endif
         endfunction
 
-        function! s:gitCheckout(branch)
+        nnoremap <silent> <Plug>(GitPushForce) :call <sid>gitPushForce()<cr>
+
+        " checkout a branch
+        function! s:gitCheckout(branch) abort
           let l:branch = substitute(substitute(a:branch, '^.*\s\+', "", ""), 'remotes/origin/', "", "")
           execute ":G switch " . l:branch
           echom "Switched to " . l:branch
         endfunction
 
-        autocmd FileType git nnoremap <buffer> w :<c-u>call <sid>gitCheckout(trim(getline(".")))<cr>
-        nnoremap <leader>n :<c-u>call <sid>gitCheckout(trim(input("branch /")))<cr>
+        nnoremap <silent> <Plug>(GitCheckoutFromInput) :<c-u>call <sid>gitCheckout(input("branch /"))<cr>
+        nnoremap <silent> <Plug>(GitCheckoutFromLine) :<c-u>call <sid>gitCheckout(trim(getline(".")))<cr>
 
-        function! s:branchFromCommit()
-          let l:commit = trim(system("git log -1 --pretty=%s"))
+        " branch off from a current commit
+        function! s:gitBranchOffFromCommit() abort
+          let l:commit = trim(system("${config.vcsExec} log -1 --pretty=%s"))
           let l:branch_name = trim(substitute(substitute(tolower(l:commit), '[^a-z0-9-]', ' ', 'g'), '\s\+', '-', 'g'))
           let l:branch_name = input("branch /", l:branch_name)
           execute ":G checkout -B " . l:branch_name
         endfunction
 
-        nnoremap <leader><C-n> :<c-u>call <sid>branchFromCommit()<cr>
+        nnoremap <silent> <Plug>(GitBranchOffFromCommit) :call <sid>gitBranchOffFromCommit()<cr>
 
-        function! s:cherryPickToBranch(branch)
-          let l:commit = trim(system("git log -1 --pretty=%H"))
+        " cherry-pick a current commit to a target branch
+        function! s:gitCherryPickToBranch(branch) abort
+          let l:commit = trim(system("${config.vcsExec} log -1 --pretty=%H"))
           execute ":G switch " . a:branch
           execute ":G pull --rebase"
           execute ":G cherry-pick " . l:commit
         endfunction
 
-        autocmd FileType git nnoremap <buffer> W :<c-u>call <sid>cherryPickToBranch(trim(getline(".")))<cr>
+        nnoremap <silent> <Plug>(GitCherryPickToBranch) :call <sid>gitCherryPickToBranch(input("branch /"))<cr>
 
+        " standard fugitive commands, autocommands and mappings
         command! -nargs=0 W Gwrite
         command! -nargs=0 E Gedit
         command! -nargs=0 D GDelete
         command! -nargs=0 Gprune call <sid>gitPruneFile()
         command! -nargs=0 Gwip call <sid>gitWIP()
         command! -nargs=0 Grebase call <sid>gitRebaseBranch()
-        command! -nargs=0 Gchanges call <sid>gitChanges()
-        command! -nargs=0 Gupdate call <sid>gitUpdate()
         command! -nargs=0 Gabsorb call <sid>gitAbsorb()
         command! -nargs=0 GabsorbAndRebase call <sid>gitAbsorbAndRebase()
-        command! -nargs=0 Gpushf call <sid>gitPush()
-        command! -nargs=1 -complete=buffer GPickaxe call <sid>pickaxe(<q-args>)
 
-        autocmd! FileType fugitive nnoremap <buffer> rI :<c-u>call <sid>gitRebaseBranch()<cr>
+        autocmd! FileType fugitive  nnoremap <buffer> rI <Plug>(GitRebaseBranch)<cr>
         autocmd! FileType gitcommit setlocal spell spelllang=en_us
         autocmd! FileType gitcommit nnoremap <buffer> <cr> <Plug>(TertiusCommitMessage)<cr>
+        autocmd! FileType git       nnoremap <buffer> w <Plug>(GitCheckoutFromLine)<cr>
+        autocmd! FileType git       nnoremap <buffer> W <Plug>(GitCherryPickToBranch)<cr>
 
         nnoremap <leader>a :Gabsorb<cr>
         nnoremap <leader>A :GabsorbAndRebase<cr>
@@ -159,16 +186,17 @@
         nnoremap <leader>I :edit .<cr>
         nnoremap <leader>e :Gedit<cr>
         nnoremap <leader>o :G fetch<cr>
-        nnoremap <leader>O :Gupdate<cr>
-        nnoremap <leader>g :call <sid>toggleFugitive()<cr>
-        nnoremap <leader>d :Gchanges<cr>
+        nnoremap <leader>O <Plug>(GitPullAndRebase)<cr>
+        nnoremap <leader>g <Plug>(GitToggleStatus)<cr>
+        nnoremap <leader>d <Plug>(GitChanges)<cr>
         nnoremap <leader>C :0GlLog --pretty=oneline<cr>
         nnoremap <leader>c :GcLog --pretty=oneline<cr>
-        nnoremap <leader>p :G pull --rebase<cr>
-        nnoremap <leader>P :G push<cr>
-        nnoremap <leader><c-p> :Gpushf<cr>
-        nnoremap <leader>s :call <sid>pickaxe(input("pickaxe /", "", "tag"))<cr>
-        vnoremap <leader>s :<c-u>call <sid>pickaxe(<sid>selectedText())<cr>
+        nnoremap <leader>p :G push<cr>
+        nnoremap <leader>P :<Plug>(GitPushForce)<cr>
+        nnoremap <leader>s <Plug>(GitPickaxeWithInput)<cr>
+        vnoremap <leader>s <Plug>(GitPickaxeWithSelection)<cr>
+        nnoremap <leader>n <Plug>(GitCheckoutFromInput)<cr>
+        nnoremap <leader><C-n> <Plug>(GitBranchOffFromCommit)<cr>
 
         " gitgutter
         nnoremap ]h :GitGutterNextHunk<cr>
