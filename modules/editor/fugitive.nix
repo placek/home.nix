247a199548345bfbd5853aed4ed14bf933a2cea4
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-07-09 09:47:57 +0200

    Move find and grep functions to fugitive module
    This commit relocates the Gind and Grep functions from the editor
    default.nix file to their respective places within the Fugitive module.
    The Fugitive module, now encapsulating these functions, enhances the
    organization and modularity of the configuration setup. By segregating
    the Gind and Grep functions, better management and independence are
    achieved for these specific editor capabilities. The changes involve
    creating and updating references in the fugitive.nix and vimrc files,
    thus improving the overall configuration structure and function
    accessibility.

--- modules/editor/fugitive.nix
+++ modules/editor/fugitive.nix
@@ -10,6 +10,8 @@
 
     editor.RCs = [
       ''
+        set wildignore+=.git/ " ignore files from git directory
+
         " Git helpers
         function! s:defaultBranch() abort
           return trim(system("${config.vcsExec} config --get core.default"))
@@ -51,8 +53,8 @@
           endif
         endfunction
 
-        nnoremap <silent> <Plug>(GitPickaxeWithInput) :<c-u>call <sid>gitPickaxe(input("pickaxe /", "", "tag"))<cr>
-        vnoremap <silent> <Plug>(GitPickaxeWithSelection) :call <sid>gitPickaxe(<sid>selectedText())<cr>
+        nnoremap <silent> <Plug>(GitPickaxe) :<c-u>call <sid>gitPickaxe(input("pickaxe /", "", "tag"))<cr>
+        vnoremap <silent> <Plug>(GitPickaxeSelected) :call <sid>gitPickaxe(<sid>selectedText())<cr>
 
         " prune a file in whole git history
         function! s:gitPruneFile(...) abort
@@ -161,6 +163,26 @@
 
         nnoremap <silent> <Plug>(GitCherryPickToBranch) :call <sid>gitCherryPickToBranch(input("branch /"))<cr>
 
+        " triggers a grep on query and opens quickfix list
+        function! s:grep(query) abort
+          if !empty(a:query)
+            execute "silent Ggrep \"" . a:query . "\" | copen"
+          endif
+        endfunction
+
+        nnoremap <silent> <Plug>(GitFind) :call <sid>find(input("find /", "", "file_in_path"))<cr>
+        vnoremap <silent> <Plug>(GitFindSelected) :call <sid>find(<sid>selectedText())<cr>
+
+        " finds a file using git ls-files or find command
+        function! s:find(query) abort
+          if !empty(a:query)
+            execute "silent cexpr system('${config.vcsExec}  ls-files \"**" . a:query . "*\" | sed \"s/.*/&:1:1/\"') | copen"
+          endif
+        endfunction
+
+        nnoremap <silent> <Plug>(GitGrep) :call <sid>grep(input("grep /", "", "tag"))<cr>
+        vnoremap <silent> <Plug>(GitGrepSelected) :call <sid>grep(<sid>selectedText())<cr>
+
         " standard fugitive commands, autocommands and mappings
         command! -nargs=0 W Gwrite
         command! -nargs=0 E Gedit
@@ -181,21 +203,26 @@
         nnoremap <leader>A :GabsorbAndRebase<cr>
         nnoremap <leader>b :G blame<cr>
         nnoremap <leader>B :G branch --all<cr>
+        nnoremap <leader>c :GcLog --pretty=oneline<cr>
+        nnoremap <leader>C :0GlLog --pretty=oneline<cr>
+        nnoremap <leader>d <Plug>(GitChanges)<cr>
+        nnoremap <leader>e :Gedit<cr>
+        nnoremap <leader>f <Plug>(GitGrep)
+        nnoremap <leader>F <Plug>(GitFind)
+        nnoremap <leader>g <Plug>(GitToggleStatus)<cr>
         nnoremap <leader>i :echo "Branch-off commit: " . <sid>branchoffCommit("HEAD")<cr>
         nnoremap <leader>I :edit .<cr>
-        nnoremap <leader>e :Gedit<cr>
-        nnoremap <leader>o :G fetch<cr>
-        nnoremap <leader>O <Plug>(GitPullAndRebase)<cr>
-        nnoremap <leader>g <Plug>(GitToggleStatus)<cr>
-        nnoremap <leader>d <Plug>(GitChanges)<cr>
-        nnoremap <leader>C :0GlLog --pretty=oneline<cr>
-        nnoremap <leader>c :GcLog --pretty=oneline<cr>
-        nnoremap <leader>p :G push<cr>
-        nnoremap <leader>P :<Plug>(GitPushForce)<cr>
-        nnoremap <leader>s <Plug>(GitPickaxeWithInput)<cr>
-        vnoremap <leader>s <Plug>(GitPickaxeWithSelection)<cr>
         nnoremap <leader>n <Plug>(GitCheckoutFromInput)<cr>
         nnoremap <leader><C-n> <Plug>(GitBranchOffFromCommit)<cr>
+        nnoremap <leader>o :G fetch<cr>
+        nnoremap <leader>O <Plug>(GitPullAndRebase)<cr>
+        nnoremap <leader>p :G push<cr>
+        nnoremap <leader>P :<Plug>(GitPushForce)<cr>
+        nnoremap <leader>s <Plug>(GitPickaxe)<cr>
+
+        vnoremap <leader>F <Plug>(GitFindSelected)<cr>
+        vnoremap <leader>f <Plug>(GitGrepSelected)<cr>
+        vnoremap <leader>s <Plug>(GitPickaxeSelected)<cr>
       ''
     ];
   };
