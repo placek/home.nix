ad2a150da80f4eec976400b8cb2f8a6d58a18b67
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-10-31 15:39:28 +0100

    Dynamically load all scripts into Greasemonkey configuration directory
    This commit modifies the script loading mechanism for the Greasemonkey
    configuration in the Qutebrowser setup. Previously, only a single script
    file was referenced explicitly. With this change, the configuration now
    dynamically loads all JavaScript files from the current directory that
    have the '.js' extension. This enhances maintainability and flexibility,
    allowing users to add or remove scripts without needing to modify the
    configuration file directly. The changes include the introduction of a
    file filter that collects relevant JavaScript files and merges them into
    the configuration, improving the way scripts are managed.

--- modules/browser/scripts/default.nix
+++ modules/browser/scripts/default.nix
@@ -1,6 +1,12 @@
 { config
+, lib
 , ...
 }:
+let
+  js_files = builtins.map (file: {
+    "qutebrowser/greasemonkey/${builtins.baseNameOf file}".source = file;
+  }) (lib.fileset.toList (lib.fileset.fileFilter (file: file.hasExt "js") ./.));
+in
 {
-  config.xdg.configFile."qutebrowser/greasemonkey/youtube-adb.js".source = ./youtube-adb.js;
+  config.xdg.configFile = lib.mkMerge js_files;
 }
