58b158ed840e014a0e74702b32f819841edf847b
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-01-20 15:54:17 +0100

    Remove Dwarf Fortress from game configurations
    This commit removes the Dwarf Fortress configuration from the Nix game
    modules. The decision to eliminate this entry was made to streamline the
    game options available to the user, focusing on titles that are
    currently supported and actively maintained. By removing unnecessary or
    unsupported games, we aim to improve the overall user experience. The
    changes involve deleting the Dwarf Fortress import statement and its
    usage in the configuration, ensuring that our game list is clean and
    relevant.

--- modules/gui/games.nix
+++ modules/gui/games.nix
@@ -33,11 +33,6 @@ let
     path = "Program Files (x86)/The Creative Assembly/Rome - Total War";
     exe  = "RomeTW-ALX.exe -show_err -mod:HRTW -noalexander";
   };
-
-  dwarf-fortress = (import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/3f888f673360440b6b7e6994a1e6d092d7588e84.tar.gz"; }) {}).pkgs.dwarf-fortress.override {
-    enableIntro = false;
-    enableFPS = true;
-  };
 in
 {
   config = {
@@ -46,7 +41,6 @@ in
       wine-rome-total-war-alexander
       wine-rome-total-war-barbarian-invasion
       wine-rome-total-war-enhancement
-      dwarf-fortress
 
       (pkgs.prismlauncher.override { jdks = [ pkgs.temurin-bin ]; })
       pkgs.wineWowPackages.stable
