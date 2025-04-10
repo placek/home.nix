2ab113c8f8b62cf2a8c03df7838f50dc1fe031fa
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-04-10 15:53:29 +0200

    Enhance marks handling in the sign column
    This commit improves the display of marks within the sign column by
    adding conditions to filter out marks that are not relevant.
    Specifically, the code now prevents marks that are either empty, or
    equal to '.' and '^' from being defined. This enhancement ensures that
    only meaningful marks are represented in the sign column, streamlining
    the user interface and reducing clutter during navigation. These changes
    aim to improve the overall usability of the editor by providing a
    clearer visualization of relevant marks.

--- modules/editor/plugins/marks.nix
+++ modules/editor/plugins/marks.nix
@@ -9,9 +9,10 @@
         function! s:displayMarksInSignColumn()
           call sign_unplace("marks")
           for mark in getmarklist(bufnr(""))
-            if substitute(mark.mark, "'", "", "g") != ""
-              execute "sign define Mark" . mark.mark . " text=" . substitute(mark.mark, "'", "", "g") . " texthl=SignColumn"
-              call sign_place(0, "marks", "Mark" . mark.mark, bufnr(""), { "lnum": mark.pos[1] })
+            let m = substitute(mark.mark, "'", "", "g")
+            if m != "" && m != '.' && m != '^'
+              execute "sign define Mark" . m . " text=" . m . " texthl=SignColumn"
+              call sign_place(0, "marks", "Mark" . m, bufnr(""), { "lnum": mark.pos[1] })
             endif
           endfor
         endfunction
