96fa2b8021455300c760258e07f9383fb333ae14
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-04-10 15:53:09 +0200

    Add key unbinding and enhanced navigation key mappings for the browser module
    This commit focuses on refining the browser's key bindings and unbinding
    certain default key combinations to improve user navigation and overall
    functionality. Specifically, it unbinds the keys 'm' and 'M' to free
    them for alternative uses. Additionally, new mappings for 'yy' and 'YY'
    are introduced for yanking selections, which enhances the clipboard
    functionality within the browser. Furthermore, navigation has been
    improved with the addition of mappings for tab management (such as 'gd'
    and 'gl') and easier access to key functionalities, ultimately
    streamlining the user experience. These changes align with the goal of
    providing a more intuitive environment for users interacting with the
    browser.

--- modules/browser/key-bindings.nix
+++ modules/browser/key-bindings.nix
@@ -9,6 +9,8 @@
 
     "pS" = "open -t -- {clipboard}";
     "ps" = "cmd-set-text /{clipboard}";
+    "yy" = "yank selection";
+    "YY" = "yank";
 
     ";D" = "hint links spawn ${config.terminalExec} -e ${config.downloaderExec} {hint-url} -d ${config.downloadsDirectory}";
     ";m" = "hint links spawn ${config.terminalExec} -e ${config.ytDownloaderExec} -x {hint-url} -o ${config.downloadsDirectory}/%(title)s.%(ext)s";
@@ -26,8 +28,16 @@
     ",ps" = "spawn --userscript qute-pass --dmenu-invocation ${config.menuExec} --no-insert-mode --password-only --password-pattern \"secret: (.+)\"";
     ",g" = "spawn --userscript GBrowse";
 
-    "<Ctrl+w>" = "mode-enter passthrough";
+    "gd" = "cmd-set-text -s :tab-take";
+    "gl" = "tab-focus last";
+
+    "<Space>l" = "tab-next";
+    "<Space>h" = "tab-prev";
+
+    "<Return>" = "mode-enter passthrough";
     "<Ctrl+h>" = "open -t https://start.duckduckgo.com/";
-    "<Ctrl+r>" = "config-cycle colors.webpage.darkmode.enabled";
+
+    "ta" = "config-cycle content.blocking.enabled";
+    "tr" = "config-cycle colors.webpage.darkmode.enabled";
   };
 }
