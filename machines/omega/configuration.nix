5b0a267c44312b5ab5035eff4b3927dc2248ca2f
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-06-17 13:10:36 +0200

    Refactor configuration for clean code
    Clean up stale PulseAudio comments and extend omega’s network settings
    to enforce DNS-based parental controls. We disable systemd-resolved and
    stop NetworkManager from overwriting /etc/resolv.conf, then enable and
    configure dnscrypt-proxy2 to listen on 127.0.0.1:53 using a NextDNS
    stamp for content filtering. Finally, we pin the system’s nameserver to
    127.0.0.1 so that all DNS requests go through dnscrypt-proxy2, ensuring
    reliable filtering.

--- machines/omega/configuration.nix
+++ machines/omega/configuration.nix
@@ -45,8 +45,6 @@
   hardware.bluetooth.enable = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.graphics.enable = true;
-#   hardware.pulseaudio.enable = true;
-#   hardware.pulseaudio.package = pkgs.pulseaudioFull;
   powerManagement.cpuFreqGovernor = "performance";
   powerManagement.enable = true;
   programs.light.enable = true;
@@ -147,7 +145,7 @@ border-width = 4px
   users.users.placek.openssh.authorizedKeys.keys = [
   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDrSau4Jlq3xQNiiEMkgETh6bU0/gSlG7ecOFOhzNrcYtcLBQzKNfJrk/59JmXNxXws3u3RBYk1oCe3xnCdeqSTpj4sLJEfXHBuGR4hk2kdk1ve+A0SxL2RKMEGUuA8v0O/0oRykv1EV3oh8HwfYVj0AQzHNxSk1H815gPGNRaq9OTJJgQvUtjNx09dtdY071rNV3D5/ozUqGczdeRbvSlSCHkLZ9mHFGJxd9lbfMV6Bs/XxHrHg+Tc3HDOSmJq7UZeX9i0kvKdyGz9qFdhuIZL4nJWrRjbAMgvMGJJxohtdqgrMv9xuz5UveNVotWBojrMU6n4UcgB1ugUkrDmDL1aBJP6zeRcgk5CtisSMt2eq69LmBEwZDWNHqVQg2Kft32urOH82VfEeZLT+sXD1kWvCFVRcmZtZlENmmkqr0axp9gf4mg1IBkyM7eXjxTg1lDeDw5yFVG/cfbtOUc+twWFJ7nFlC6wVE5prnRW+qI6gpGB4gGZVtzODmIT4OeTXKI2MZPTMn2pwjmx3NM3p8ofZawr3c8TZwCStuWiIvoes3Ps4kt2Z75hoZ+4+LEucUwop0jees0YxrNoFTbwdbfXH0mBCspeSS65CZ96Og2qdE7s1+t3tdZrBWPmgziZIPtvBAmYmzH9JKAX1JgmRirf4tG5sZ2JbA8WDUqSADmadw== cardno:000611879902"
   ];
-  
+
   ################################# NETWORK ####################################
   networking.hostName = "omega";
   networking.hosts."127.0.0.1" = ["localhost" "dev"];
@@ -168,11 +166,7 @@ border-width = 4px
     settings = {
       listen_addresses = [ "127.0.0.1:53" ]; # Ensure it handles DNS on localhost
       server_names = [ "NextDNS-a4eea8" ];
-      static = {
-        "NextDNS-a4eea8" = {
-          stamp = "sdns://AgEAAAAAAAAAAAAOZG5zLm5leHRkbnMuaW8HL2E0ZWVhOA";
-        };
-      };
+      static."NextDNS-a4eea8".stamp = "sdns://AgEAAAAAAAAAAAAOZG5zLm5leHRkbnMuaW8HL2E0ZWVhOA";
     };
   };
 
