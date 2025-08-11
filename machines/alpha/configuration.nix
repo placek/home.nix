25ff7b4566bdb73e8325ce20691d07e7af2eed7b
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-08-11 06:42:42 +0200

    Remove Avahi service from alpha and omega configurations
    Avahi mDNS service is no longer required in our deployments. This change
    removes the redundant `services.avahi` blocks from both
    `machines/alpha/configuration.nix` and
    `machines/omega/configuration.nix`, streamlining the NixOS
    configurations and reducing unnecessary services.

--- machines/alpha/configuration.nix
+++ machines/alpha/configuration.nix
@@ -143,16 +143,6 @@
     };
   };
 
-  services.avahi = {
-    enable = true;          # Enable Avahi daemon
-    nssmdns = true;         # Enable resolving .local names
-    publish = {
-      enable = true;
-      addresses = true;
-      workstation = true;   # Advertise host as workstation
-    };
-  };
-
   ################################# TRAEFIK ####################################
   services.traefik = {
     enable = true;
