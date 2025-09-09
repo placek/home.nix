f5cf682c4981674c1f62eff7d286b57a046875b8
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-09-09 15:15:18 +0200

    Move hyprland bindings to module

---
+++ modules/gui/hyprland/bindings.nix
@@ -0,0 +1,93 @@
+{ pkgs
+, config
+, ...
+}:
+let
+  cliphistMenu = ''
+    ${pkgs.cliphist}/bin/cliphist list | ${pkgs.wofi}/bin/wofi --dmenu | ${pkgs.cliphist}/bin/cliphist decode | ${pkgs.wl-clipboard}/bin/wl-copy
+  '';
+  udisksMenu = ''
+    ${pkgs.udiskie}/bin/udiskie-umount -a; ${pkgs.udiskie}/bin/udiskie-mount -a
+  '';
+in
+{
+  config = {
+    wayland.windowManager.hyprland.settings = {
+      "$mod" = "SUPER";
+
+      binds.pass_mouse_when_bound = false;
+      binds.drag_threshold = 10;
+
+      bindm = [
+        # Move/Resize floating windows with mouse + SUPER
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+      bind = [
+        # FOCUS
+        # Windows
+        "$mod, j, cyclenext"
+        "$mod, k, cyclenext, prev"
+        # Workspaces
+        "$mod CTRL, j, workspace, r+1"
+        "$mod CTRL, k, workspace, r-1"
+        # Monitors
+        "$mod SHIFT, j, focusmonitor, +1"
+        "$mod SHIFT, k, focusmonitor, -2"
+
+        # MOVE
+        # Reorder windows
+        "$mod, h, swapnext"
+        "$mod, l, swapnext, prev"
+        # Between workspaces
+        "$mod CTRL, h, movetoworkspace, r-1"
+        "$mod CTRL, l, movetoworkspace, r+1"
+        # Between monitors
+        "$mod SHIFT, h, movewindow, mon:-1"
+        "$mod SHIFT, l, movewindow, mon:+1"
+
+        # Toggle floating
+        "$mod, mouse:274, togglefloating"
+
+        # Shrink/Expand master area (Ctrl+j/k)
+        "$mod CTRL SHIFT, h, layoutmsg, mfact -0.02"
+        "$mod CTRL SHIFT, l, layoutmsg, mfact +0.02"
+        "$mod, mouse_down, layoutmsg, mfact -0.02"
+        "$mod, mouse_up,   layoutmsg, mfact +0.02"
+
+        # Close / Quit (q / Shift+q)
+        "$mod, q, killactive"
+        "$mod SHIFT, q, exec, hyprctl dispatch exit"
+
+        # Spawn terminal / launcher / pass / clipboard / udisks
+        "$mod, Return, exec, ${config.terminalExec}"
+        "$mod, space,  exec, ${pkgs.wofi}/bin/wofi --show drun"
+        "$mod, b,      exec, ${pkgs.wofi-pass}/bin/wofi-pass"
+#           "$mod, n,      exec, ${cliphistMenu}"
+#         "$mod, m,      exec, sh -c '${udisksMenu}'"
+
+        # Notifications: history “pop” (panel) / toggle DND
+        "$mod, Escape, exec, ${pkgs.dunst}/bin/dunstctl history-pop"
+#           "$mod CTRL, Escape, exec, ${swayncctl} --toggle-dnd"
+
+        # Multimedia
+        ", Print, exec, ${pkgs.hyprshot}/bin/hyprshot -m region -o ${config.downloadsDirectory} -f 'screenshot_%Y-%m-%d_%H-%M-%S.png' -c"
+        # Player
+        ", XF86AudioPrev,  exec, ${pkgs.playerctl}/bin/playerctl previous"
+        ", XF86AudioPlay,  exec, ${pkgs.playerctl}/bin/playerctl play-pause"
+        ", XF86AudioNext,  exec, ${pkgs.playerctl}/bin/playerctl next"
+        # Volume via PipeWire
+        ", XF86AudioMute,         exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioLowerVolume,  exec, ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioRaiseVolume,  exec, ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0"
+        # Sleep/PowerOff keys → lock (like slock)
+        ", XF86Sleep,    exec, ${pkgs.hyprlock}/bin/hyprlock"
+        ", XF86PowerOff, exec, ${pkgs.hyprlock}/bin/hyprlock"
+
+        # TODO
+        # Next layout (BackSpace)
+        "$mod SHIFT, BackSpace, layoutmsg, cyclenext"
+      ];
+    };
+  };
+}
--- modules/gui/hyprland/default.nix
+++ modules/gui/hyprland/default.nix
@@ -3,38 +3,19 @@
 , pkgs
 , ...
 }:
-
-let
-  # Wayland-native replacements / helpers
-  passmenu = "${pkgs.wofi-pass}/bin/wofi-pass";          # pass prompt replacement
-  cliphistMenu = ''
-    ${pkgs.cliphist}/bin/cliphist list | ${pkgs.wofi}/bin/wofi --dmenu | ${pkgs.cliphist}/bin/cliphist decode | ${pkgs.wl-clipboard}/bin/wl-copy
-  '';
-  udisksMenu = ''
-    ${pkgs.udiskie}/bin/udiskie-umount -a; ${pkgs.udiskie}/bin/udiskie-mount -a
-  '';
-  # Notifications: swaync (has history & DND like dunst)
-  swayncctl = "${pkgs.swaynotificationcenter}/bin/swaync-client";
-
-  # Colors from your scheme
-  fontName   = "${config.gui.font.name}";
-  fontSize   = builtins.toString config.gui.font.size;
-
-  # Show last notification (approximate “history-pop”): open notification center
-  notifHistoryPop = "${swayncctl} -t";  # toggle panel (closest UX to popping last)
-in
 {
   imports = [
     ./wallpaper
     ./waybar
     ./wofi
+    ./bindings.nix
+    ./cursor.nix
   ];
 
   config = {
     home.packages = with pkgs; [
-      wofi-pass cliphist wl-clipboard
-      udiskie
-      waybar
+      wl-clipboard
+#       udiskie
     ];
 
     # Start services/utilities on session start
@@ -69,84 +50,8 @@ in
 
         exec-once = [
           "${pkgs.swaynotificationcenter}/bin/swaync &"
-          "${pkgs.cliphist}/bin/cliphist store &"
-          "${pkgs.waybar}/bin/waybar &"
-        ];
-
-        "$mod" = "SUPER";
-
-        binds.pass_mouse_when_bound = false;
-        binds.drag_threshold = 10;
-
-        bindm = [
-          # Move/Resize floating windows with mouse + SUPER
-          "$mod, mouse:272, movewindow"
-          "$mod, mouse:273, resizewindow"
-        ];
-        bind = [
-          # FOCUS
-          # Windows
-          "$mod, j, cyclenext"
-          "$mod, k, cyclenext, prev"
-          # Workspaces
-          "$mod CTRL, j, workspace, r+1"
-          "$mod CTRL, k, workspace, r-1"
-          # Monitors
-          "$mod SHIFT, j, focusmonitor, +1"
-          "$mod SHIFT, k, focusmonitor, -2"
-
-          # MOVE
-          # Reorder windows
-          "$mod, h, swapnext"
-          "$mod, l, swapnext, prev"
-          # Between workspaces
-          "$mod CTRL, h, movetoworkspace, r-1"
-          "$mod CTRL, l, movetoworkspace, r+1"
-          # Between monitors
-          "$mod SHIFT, h, movewindow, mon:-1"
-          "$mod SHIFT, l, movewindow, mon:+1"
-
-          # Toggle floating
-          "$mod, mouse:274, togglefloating"
-
-          # Shrink/Expand master area (Ctrl+j/k)
-          "$mod CTRL SHIFT, h, layoutmsg, mfact -0.02"
-          "$mod CTRL SHIFT, l, layoutmsg, mfact +0.02"
-          "$mod, mouse_down, layoutmsg, mfact -0.02"
-          "$mod, mouse_up,   layoutmsg, mfact +0.02"
-
-          # Close / Quit (q / Shift+q)
-          "$mod, q, killactive"
-          "$mod SHIFT, q, exec, hyprctl dispatch exit"
-
-          # Spawn terminal / launcher / pass / clipboard / udisks
-          "$mod, Return, exec, ${config.terminalExec}"
-          "$mod, space,  exec, ${pkgs.wofi}/bin/wofi --show drun"
-          "$mod, b,      exec, ${passmenu}"
-#           "$mod, n,      exec, ${cliphistMenu}"
-  #         "$mod, m,      exec, sh -c '${udisksMenu}'"
-
-          # Notifications: history “pop” (panel) / toggle DND
-          "$mod, Escape, exec, ${pkgs.dunst}/bin/dunstctl history-pop"
-#           "$mod CTRL, Escape, exec, ${swayncctl} --toggle-dnd"
-
-          # Multimedia
-          ", Print, exec, ${pkgs.hyprshot}/bin/hyprshot -m region -o ${config.downloadsDirectory} -f 'screenshot_%Y-%m-%d_%H-%M-%S.png' -c"
-          # Player
-          ", XF86AudioPrev,  exec, ${pkgs.playerctl}/bin/playerctl previous"
-          ", XF86AudioPlay,  exec, ${pkgs.playerctl}/bin/playerctl play-pause"
-          ", XF86AudioNext,  exec, ${pkgs.playerctl}/bin/playerctl next"
-          # Volume via PipeWire
-          ", XF86AudioMute,         exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-          ", XF86AudioLowerVolume,  exec, ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-          ", XF86AudioRaiseVolume,  exec, ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0"
-          # Sleep/PowerOff keys → lock (like slock)
-          ", XF86Sleep,    exec, ${pkgs.hyprlock}/bin/hyprlock"
-          ", XF86PowerOff, exec, ${pkgs.hyprlock}/bin/hyprlock"
-
-          # TODO
-          # Next layout (BackSpace)
-          "$mod SHIFT, BackSpace, layoutmsg, cyclenext"
+#           "${pkgs.cliphist}/bin/cliphist store &"
+          "${config.programs.waybar.package}/bin/waybar &"
         ];
       };
     };
