ddeecad79533f6a49849ac7b0e2e97097b4785d5
Paweł Placzyński <placzynski.pawel@gmail.com>
2026-01-16 07:25:55 +0100

    Add database initialization to PostgreSQL configuration
    Ensure initial setup of crucial database and users in PostgreSQL to facilitate application connectivity and data management, enhancing deployment efficiency.

--- machines/alpha/configuration.nix
+++ machines/alpha/configuration.nix
@@ -277,6 +277,12 @@ in
       host    all             all             127.0.0.1/32            scram-sha-256
       host    all             all             ::1/128                 scram-sha-256
     '';
+
+    ensureDatabases = [ "bible" ];
+    ensureUsers = [
+      { name = "postgrest"; }
+      { name = "anon"; }
+    ];
   };
 
   services.pgadmin = {
