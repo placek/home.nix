ccc496bf2db1b98a69dd5363805c14059a2a21dc
Paweł Placzyński <placzynski.pawel@gmail.com>
2026-04-08 08:40:35 +0200

    Add English spell check configuration
    Support English spell checking by adding a separate configuration for the 'en.utf-8' language, alongside the existing Polish configuration. This enables users to enable spell checking for both languages independently.

--- modules/editor/plugins/spell.nix
+++ modules/editor/plugins/spell.nix
@@ -14,15 +14,20 @@ let
     language = "pl.utf-8";
     sha256 = "sha256-FM5QZEzT9p1adhtptZOKbQMYIytctqgB3DTCPaWF5+k=";
   };
+
+  englishSpell = vimSpellFile {
+    language = "en.utf-8";
+    sha256 = "sha256-/sq9yUm2o50ywImfolReqyXmPy7QozxK0VEUJjhNMHA=";
+  };
 in
 {
   config = {
     home.file.".config/vim/spell/pl.utf-8.spl".source = polishSpell;
+    home.file.".config/vim/spell/en.utf-8.spl".source = englishSpell;
     editor.RCs = [
       ''
         set runtimepath+=~/.config/vim
         set spelllang=pl
-        set spell
       ''
     ];
   };
