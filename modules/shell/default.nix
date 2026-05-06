3d56f4609c9ba376eeee98dc8e2c9152bfe35460
Paweł Placzyński <placzynski.pawel@gmail.com>
2026-05-06 06:13:22 +0200

    Refactor shell configuration to separate Fish into dedicated fish.nix module
    This commit extracts Fish shell-specific configuration into its own module (fish.nix) for better code organization and maintainability. The previous default configuration now focuses on general shells, making it easier to extend and manage different shell environments independently.

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -15,61 +15,14 @@
 
   imports = [
     ./prompt.nix
+    ./fish.nix
     ./bash.nix
   ];
 
   config = {
-    home.sessionVariables.SHELL = "fish";
-
-    programs.fzf.enableFishIntegration = true;            # fuzzy finder
-    programs.nix-index.enableFishIntegration = true;      # nix package search
-    programs.zoxide.enableFishIntegration = true;         # directory navigation
-    programs.nix-your-shell.enableFishIntegration = true; # nix shell management
     programs.lsd.enable = true;
-    programs.lsd.enableFishIntegration = true;            # replacement for ls
-    programs.carapace.enable = true;                      # shell completion
-    programs.carapace.enableFishIntegration = true;
-    programs.mcfly.enable = true;                         # command history
-    programs.mcfly.enableFishIntegration = true;
-    programs.starship.enableFishIntegration = true;
+    programs.carapace.enable = true;
+    programs.mcfly.enable = true;
     programs.zoxide.enable = true;
-
-    services.gpg-agent.enableFishIntegration = true;
-
-    programs.fish = {
-      enable = true;
-      interactiveShellInit = ''
-        bind ctrl-x echo\ -n\ \(clear\ \|\ string\ replace\ \\e\\\[3J\ \"\"\)\;\ commandline\ -f\ repaint
-        bind ctrl-a fzf-cd-widget
-        bind ctrl-z undo
-        bind f11 accept-autosuggestion
-      '';
-
-      functions.fish_greeting.body = "";
-      shellAbbrs = {
-        dsp = "docker system prune";
-        dspv = "docker system prune --volumes";
-        dspa = "docker system prune --volumes --all";
-
-        dcb = "docker compose build";
-        dce = "docker compose exec";
-        dca = "docker compose attach";
-        dcr = "docker compose run --rm";
-        dcu = "docker compose up --detach --remove-orphans";
-        dcl = "docker compose logs";
-        dcd = "docker compose down --remove-orphans";
-        dcdv = "docker compose down --remove-orphans --volumes";
-        dcres = "docker compose restart";
-        dcps = "docker compose ps";
-
-        j = "journalctl";
-        s = "systemctl";
-
-        t = "tldr";
-
-        sshh = "TERM=xterm-256color ssh";
-        cdt = "cd (mktemp -d)";
-      };
-    };
   };
 }
---
+++ modules/shell/fish.nix
@@ -0,0 +1,57 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+{
+  config = {
+    home.sessionVariables.SHELL = "fish";
+
+    programs.fzf.enableFishIntegration = true;            # fuzzy finder
+    programs.nix-index.enableFishIntegration = true;      # nix package search
+    programs.zoxide.enableFishIntegration = true;         # directory navigation
+    programs.nix-your-shell.enableFishIntegration = true; # nix shell management
+    programs.lsd.enableFishIntegration = true;            # replacement for ls
+    programs.carapace.enableFishIntegration = true;
+    programs.mcfly.enableFishIntegration = true;
+    programs.starship.enableFishIntegration = true;
+
+    services.gpg-agent.enableFishIntegration = true;
+
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        bind ctrl-x echo\ -n\ \(clear\ \|\ string\ replace\ \\e\\\[3J\ \"\"\)\;\ commandline\ -f\ repaint
+        bind ctrl-a fzf-cd-widget
+        bind ctrl-z undo
+        bind f11 accept-autosuggestion
+      '';
+
+      functions.fish_greeting.body = "";
+      shellAbbrs = {
+        dsp = "docker system prune";
+        dspv = "docker system prune --volumes";
+        dspa = "docker system prune --volumes --all";
+
+        dcb = "docker compose build";
+        dce = "docker compose exec";
+        dca = "docker compose attach";
+        dcr = "docker compose run --rm";
+        dcu = "docker compose up --detach --remove-orphans";
+        dcl = "docker compose logs";
+        dcd = "docker compose down --remove-orphans";
+        dcdv = "docker compose down --remove-orphans --volumes";
+        dcres = "docker compose restart";
+        dcps = "docker compose ps";
+
+        j = "journalctl";
+        s = "systemctl";
+
+        t = "tldr";
+
+        sshh = "TERM=xterm-256color ssh";
+        cdt = "cd (mktemp -d)";
+      };
+    };
+  };
+}
